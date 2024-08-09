cb1ad5d4109d391563380f1d9ac7938a31c47e56
siddharthbahl <bahlsiddharth123@gmail.com>
2024-08-09 03:30:30 +0100

    Adding support for variables

--- src/main/java/com/lang/benzene/Interpreter.java
+++ src/main/java/com/lang/benzene/Interpreter.java
@@ -5,6 +5,8 @@ import static src.main.java.com.lang.benzene.TokenType.SLASH;
 import java.util.List;;
 
 public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
+    private Environment environment = new Environment();
+
     void interpret(List<Stmt> statements){
         try {
             for (Stmt stmt : statements){
@@ -27,6 +29,26 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         return null;
 
     }
+
+    @Override
+    public Void visitVarStmt(Stmt.Var stmt){
+        Object value = null;
+        if (stmt.initializer != null){
+            value = evaluate(stmt.initializer);
+        }
+
+        environment.define(stmt.name.lexeme, value);
+        return null;
+    }
+
+    @Override
+    public Object visitAssignExpr(Expr.Assign expr){
+        Object value = evaluate(expr);
+        environment.assign(expr.name, value);
+
+        return value;
+    }
+
     @Override
     public Object visitLiteralExpr(Expr.Literal expr){
         return expr.value;
@@ -50,7 +72,11 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         }
 
         return null;
+    }
 
+    @Override
+    public Object visitVariableExpr(Expr.Variable expr){
+        return environment.get(expr.name);
     }
 
     private void checkNumberOperand(Token operator, Object operand){
